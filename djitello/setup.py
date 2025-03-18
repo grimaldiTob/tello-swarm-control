136e82410d0eca7c0825c76c40e590511f8e4586
tobia <forzamilan688@gmail.com>
2025-03-18 18:55:31 +0100

    lab 18/03

--- djitello/setup.py
+++ djitello/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'tello'
+package_name = 'djitello'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "tellos = tello.main:main"
+            "init = tello.main:main"
         ],
     },
 )
