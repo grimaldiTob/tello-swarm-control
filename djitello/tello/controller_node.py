561c15335edd4901282203aca5617a3ffc1b1083
tobia <forzamilan688@gmail.com>
2025-03-18 18:54:57 +0100

    lab 18/03

--- djitello/tello/controller_node.py
+++ djitello/tello/controller_node.py
@@ -1,17 +1,16 @@
-import rclpy
-from rclpy import Node
+from rclpy.node import Node
 from std_msgs.msg import String
 
 # nodo di controllo, invia i comandi ai singoli nodi
 class SwarmNode(Node):
 
     def __init__(self):
-        super().__init__()
+        super().__init__("Controller")
         self.get_logger().info("Inizializzando lo swarm")
-        self.publishers = [] # lista vuota di publishers
+        self.publisher_ = []
 
     # creo i publisher in base agli n tello nello swarm
     def init_publishers(self, n:int):
         for i in range(n):
-            self.publishers[i] = self.create_publisher(String, "/Command/tello" + i, 10)
-            self.get_logger().info("Creato pubblisher al topic /Command/tello " + i)
+            #self.publisher_[i] = self.create_publisher(String, "/Command/tello" + str(i), 10)
+            self.get_logger().info("Creato pubblisher al topic /Command/tello " + str(i))
--- djitello/tello/main.py
+++ djitello/tello/main.py
@@ -1,21 +1,22 @@
-from djitellopy import TelloSwarm
-from tello_node import TelloNode
-from controller_node import SwarmNode
+from djitellopy import Tello
+from tello.tello_node import TelloNode
+from tello.controller_node import SwarmNode
 import rclpy
 
-FILE_PATH = "resource/ips.txt"
+FILE_PATH = "/ws/src/djitello/resource/ips.txt"
 
-swarm = TelloSwarm.fromFile(FILE_PATH)
-
-# lista di nodi tello
-tello_nodes = []
+#swarm = TelloSwarm.fromFile(FILE_PATH)
 
 def main():
-    for i in enumerate(swarm.tellos):
-        tello_nodes[i] = TelloNode(swarm.tellos[i], str(i+1))
-    n = len(swarm.tellos)
+    rclpy.init()
+    tello_nodes = [TelloNode(Tello("192.168.16.112"), "1"), TelloNode(Tello("192.168.16.113"), "2")]
+    n = len(tello_nodes)
+    rclpy.spin_once(tello_nodes[0])
+    rclpy.spin_once(tello_nodes[1])
     controller = SwarmNode()
-    controller.init_publishers(n)
+    #controller.init_publishers(n)
+    #rclpy.spin(controller)
+    rclpy.shutdown()
 
  
 
